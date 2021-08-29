768f6abc92af614423ab7db0fe304153b60731f5
khanhln <khanhln1570@gmail.com>
2021-08-29 23:33:19 +0700

    Update home.controller.js

--- controller/home.controller.js
+++ controller/home.controller.js
@@ -6,7 +6,7 @@ module.exports.index = async(req, res) => {
     const notis = await prisma.notification.findMany({ orderBy: { notiId: 'desc' } })
     const newsList = await prisma.news_event.findMany({ orderBy: { newsId: 'desc' } })
     res.render('index', {
-        title: 'Trang chủ',
+        title: 'Ngôi trường đáng tin cậy!',
         teachers,
         notis,
         newsList
