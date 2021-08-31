5dc4ea012f3ccde0b8abb38ed2cabcd04e23e45a
khanhln <khanhln1570@gmail.com>
2021-08-31 09:44:42 +0700

    update 6

--- controller/home.controller.js
+++ controller/home.controller.js
@@ -3,8 +3,8 @@ const prisma = new PrismaClient();
 
 module.exports.index = async(req, res) => {
     const teachers = await prisma.member.findMany();
-    const notis = await prisma.notification.findMany({ orderBy: { notiId: 'desc' } })
-    const newsList = await prisma.news_event.findMany({ orderBy: { newsId: 'desc' } })
+    const notis = await prisma.notification.findMany({ take: 3, orderBy: { notiId: 'desc' } })
+    const newsList = await prisma.news_event.findMany({ take: 3, orderBy: { newsId: 'desc' } })
     res.render('index', {
         title: 'Ngôi trường đáng tin cậy!',
         teachers,
--- controller/news-event.controller.js
+++ controller/news-event.controller.js
@@ -20,7 +20,18 @@ module.exports.getOne = async(req, res) => {
         }
 
     });
+    var content = news.content.split(" ");
+    console.log(content)
+    var links = [];
+    content.forEach(element => {
+        if (element.startsWith('http') || element.startsWith(' http') ||
+            element.startsWith('\nhttp') || element.startsWith('\rhttp')) {
+            element.replace(/\r?\n|\r|\n|\n*$/, "")
+            links.push(element)
+        }
+    });
 
+    console.log(links)
     if (!news) {
         res.redirect('/page404')
     } else {
@@ -28,7 +39,8 @@ module.exports.getOne = async(req, res) => {
         res.render('news-events/news-event-detail', {
             title: 'Chi tiết Tin tức - Sự kiện',
             newss,
-            news
+            news,
+            links
 
         });
     }
