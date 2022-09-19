df17cd1d622c395c5724d85d220b476ddb3042e2
khanhln <khanhln1570@gmail.com>
2022-09-19 11:25:36 +0700

    Update notification.controller.js

--- controller/notification.controller.js
+++ controller/notification.controller.js
@@ -1,81 +1,98 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-module.exports.index = async(req, res) => {
-    const notis = await prisma.notification.findMany({
-        orderBy: {
-            createDate: 'desc'
-        }
+module.exports.index = async (req, res) => {
+  const notis = await prisma.notification.findMany({
+    orderBy: {
+      createDate: "desc",
+    },
+    where: {
+      NOT: {
+        OR: [
+          {
+            title: {
+              startsWith: "Công khai",
+            },
+          },
+          {
+            title: {
+              startsWith: "CÔNG KHAI",
+            },
+          },
+        ],
+      },
+    },
+  });
+  res.render("notifications/notification", {
+    title: "Thông Báo",
+    notis,
+  });
+};
+
+module.exports.getOne = async (req, res) => {
+  var id = parseInt(req.params.id);
+  const notis = await prisma.notification.findMany();
+  const noti = await prisma.notification.findUnique({
+    where: { notiId: id },
+    include: {
+      image: true,
+      video: true,
+    },
+  });
+
+  if (!noti) {
+    return res.redirect("/page404");
+  } else {
+    var content = noti.content.split(" ");
+    // console.log(content)
+    var links = [];
+    content.forEach((element) => {
+      if (
+        element.startsWith("http") ||
+        element.startsWith(" http") ||
+        element.startsWith("\nhttp") ||
+        element.startsWith("\rhttp") ||
+        element.startsWith("(http") ||
+        element.startsWith("[http") ||
+        element.startsWith("{http")
+      ) {
+        var link = element.replace(/[{()}\[\]]/g, "");
+
+        links.push(link);
+      }
     });
-    res.render('notifications/notification', {
-        title: 'Thông Báo',
-        notis
+    // console.log(links);
+    return res.render("notifications/notification-detail", {
+      title: "Chi tiết Thông Báo",
+      noti,
+      notis,
+      links,
     });
-}
+  }
+};
 
-module.exports.getOne = async(req, res) => {
-
-    var id = parseInt(req.params.id);
-    const notis = await prisma.notification.findMany();
-    const noti = await prisma.notification.findUnique({
-        where: { notiId: id },
-        include: {
-            image: true,
-            video: true
-        }
-
-    });
-
-    if (!noti) {
-        return res.redirect('/page404')
-    } else {
-        var content = noti.content.split(" ");
-        // console.log(content)
-        var links = [];
-        content.forEach(element => {
-            if (element.startsWith('http') || element.startsWith(' http') ||
-                element.startsWith('\nhttp') || element.startsWith('\rhttp') || element.startsWith('(http') ||
-                element.startsWith('[http') ||
-                element.startsWith('{http')) {
-                var link = element.replace(/[{()}\[\]]/g, '')
-
-                links.push(link)
-            }
-        });
-        // console.log(links);
-        return res.render('notifications/notification-detail', {
-            title: 'Chi tiết Thông Báo',
-            noti,
-            notis,
-            links
-
-        });
-    }
-
-}
-
-module.exports.indexPublic = async(req, res) => {
-    const notis = await prisma.notification.findMany({
-        orderBy: {
-            createDate: 'desc'
+module.exports.indexPublic = async (req, res) => {
+  const notis = await prisma.notification.findMany({
+    orderBy: {
+      createDate: "desc",
+    },
+    where: {
+      OR: [
+        {
+          title: {
+            startsWith: "Công khai",
+          },
         },
-        where:{
-            OR:[
-                {
-                    title:{
-                        startsWith: 'Công khai'
-                    }
-                },
-                {
-                    title:{
-                        startsWith: 'CÔNG KHAI'
-                    }
-                },
-            ]
-        }
-    });
-    res.render('notifications/notification-public', {
-        title: 'Công Khai',
-        notis
-    });
-}
+        {
+          title: {
+            startsWith: "CÔNG KHAI",
+          },
+        },
+      ],
+    },
+  });
+  res.render("notifications/notification-public", {
+    title: "Công Khai",
+    notis,
+  });
+};
